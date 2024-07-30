6b9593e95dedd448b27df2daa18596dcf8a9bc80
DanieleChecchin <danielechecchin26@gmail.com>
2024-07-30 14:13:39 +0200

    scaletta

---
+++ script.js
@@ -0,0 +1,18 @@
+// SCALETTA
+
+/*
+1- Recupero gli elementi del DOM di mio interesse
+2- Chiedo il nome all'utente
+3- Chiedo il cognome all'utente
+4- Chiedo il colore preferito all'utente
+5- Stampo nel DOM aggiungendo '21'
+
+*/
+
+// Fase preparatoria
+
+// Fase raccolta dati
+
+// Fase lavorazione dati
+
+// Fase produzione output

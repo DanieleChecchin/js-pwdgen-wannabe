2290187e7e245da869cf1925ac0a32882e618284
DanieleChecchin <danielechecchin26@gmail.com>
2024-07-30 14:37:59 +0200

    completato + bonus

--- script.js
+++ script.js
@@ -9,16 +9,17 @@
 
 */
 
-// Fase preparatoria
-
+// ! Fase preparatoria
 const passwordElement = document.getElementById('password');
 console.log(passwordElement);
 
-// Fase raccolta dati
-const firstName = prompt('Inserire il nome', 'Daniele');
-const lastName = prompt('Inserire il cognome', 'Checchin');
-const colour = prompt('Inserire il colore preferito', 'Viola');
+// ! Fase raccolta dati
+const firstName = prompt('Inserire il nome', 'daniele');
+const lastName = prompt('Inserire il cognome', 'checchin');
+const colourFavourite = prompt('Inserire il colore preferito', 'viola');
 
-// Fase lavorazione dati
+// ! Fase lavorazione dati
+const message = `${firstName}${lastName}${colourFavourite}21`;
 
-// Fase produzione output
+// ! Fase produzione output
+passwordElement.innerText = message;
